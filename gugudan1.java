8746a4ec75fd62ddd9225a148bd8592c6afff537
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2020-12-23 19:55:23 +0900

    변수를 사용한 4단 타이핑

---
+++ gugudan1.java
@@ -0,0 +1,24 @@
+
+public class gugudan1 {
+	public static void main(String[] args) {
+		System.out.println("4단");
+		int result = 4 * 1 ;
+		System.out.println(result);
+		result = 4 * 2 ;
+		System.out.println(result);
+		result = 4 * 3 ;
+		System.out.println(result);
+		result = 4 * 4 ;
+		System.out.println(result);
+		result = 4 * 5 ;
+		System.out.println(result);
+		result = 4 * 6 ;
+		System.out.println(result);
+		result = 4 * 7 ;
+		System.out.println(result);
+		result = 4 * 8 ;
+		System.out.println(result);
+		result = 4 * 9 ;
+		System.out.println(result);
+	}
+}
