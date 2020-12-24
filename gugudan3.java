e9cf29b9f17c8b3bd9f21b18a70b6b1a1401a946
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2020-12-24 19:36:28 +0900

    반복문 While를 사용한 구구단

---
+++ gugudan3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class gugudan3 {
+	public static void  main(String[] args) {
+		System.out.println("몇 단을 출력할까요?");
+		Scanner scan = new Scanner(System.in);
+		int i = 1;
+		int n = scan.nextInt();
+		while(i < 10) {
+			System.out.println(n + " x " + i + " = " + i);
+			i++;
+		}
+	}
+}
